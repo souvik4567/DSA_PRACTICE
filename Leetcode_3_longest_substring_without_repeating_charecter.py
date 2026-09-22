a4bf7582afb364b8f88254543346db145628a6fe
souvik4567 <souvik4567@gmail.com>
2026-09-23 00:45:18 +0530

    GENAI=NO| Added Hashmat approach also

--- Leetcode_3_longest_substring_without_repeating_charecter.py
+++ Leetcode_3_longest_substring_without_repeating_charecter.py
@@ -23,3 +23,16 @@ class Solution(object):
                 seen.remove(s[left])
                 left +=1 
         return max_length
+
+                seen = {}
+        '''
+        #hash Map approach
+        
+        left, max_length = 0, 0
+        for i,ch in enumerate(s):
+            if ch in seen and seen[ch]>=left:                 # this check is requred to make sure we are checking under window or not
+                left = seen[ch]+1
+            seen[ch] = i    
+            max_length = max(i-left+1,max_length)
+        return max_length
+        '''
