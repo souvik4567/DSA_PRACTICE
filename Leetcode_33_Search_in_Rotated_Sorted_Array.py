403f8098eb98632adcdb525d1826230b1c8cd0a8
souvik4567 <souvik4567@gmail.com>
2026-09-27 02:06:16 +0530

    GENAI=NO|Leetcode_33_Search_in_Rotated_Sorted_Array

---
+++ Leetcode_33_Search_in_Rotated_Sorted_Array.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
+        left = 0
+        right = len(nums) - 1
+
+        mid = (left + right ) // 2
+
+        while left <= right :
+            
+            mid = (left + right ) // 2
+            print(mid)
+            if target == nums [mid] :
+                return mid 
+            
+
+            if nums[mid] >= nums [left] :
+                if target < nums[left] or target > nums[mid]:
+                    left =  mid +1
+                else :
+                    right = mid - 1
+            else :
+                if target < nums[mid] or target > nums[right]:
+                    right = mid - 1
+                else :
+                    left =  mid +1
+        return -1
+
